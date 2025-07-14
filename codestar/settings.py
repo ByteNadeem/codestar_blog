77aea4c4f8d8c077a1f0e2d5831e6d44874c052d
ByteNadeem <nadeemzachav@hotmail.com>
2025-07-14 20:55:28 +0100

    readies code for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m6#c_^+_%wot4u8u2#l_u%o+3@fhbqh41n4j2jop=zy0bz*39r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [,'.herokuapp.com']
 
 
 # Application definition
